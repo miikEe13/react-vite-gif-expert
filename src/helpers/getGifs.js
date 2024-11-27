9c74bb1d77792b7a5ec91a1dddd7fe6f5a36f8b0
Miguel Angel Ramirez Medel <masramirez12@gmail.com>
2024-11-27 17:18:54 -0600

    Fixes

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async( category ) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=GY1SHZZZMfRzMSdWyuOoxAcDuzZnTahK&q=${ category }&limit=8`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=GY1SHZZZMfRzMSdWyuOoxAcDuzZnTahK&q=${ category }&limit=12`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
